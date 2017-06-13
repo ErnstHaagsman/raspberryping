220699c57fd8498e56427f199f51230e33c42ccb
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-06-13 13:33:58 +0200

    Use Ping objects

--- ping.py
+++ ping.py
@@ -8,6 +8,8 @@ import psycopg2
 import re
 import subprocess32
 
+from pingmodel import Ping
+
 if len(sys.argv) != 2:
     print("Usage: ping.py [host]")
     exit(1)
@@ -16,16 +18,18 @@ host = sys.argv[1]
 user = os.getlogin()
 
 # Do the pings
-pings = subprocess32.check_output(["ping", host, "-c 5"])
+ping_output = subprocess32.check_output(["ping", host, "-c 5"])
+pings = []
 
-for line in pings.split('\n'):
+for line in ping_output.split('\n'):
     if re.match("\d+ bytes from", line):
         bytes_received = line.split()[0]
         parts = re.search('ttl=(\d+) time=(\d+\.\d+) ms', line.split(':')[1])
         ttl = parts.group(1)
         time = parts.group(2)
 
-        print('PING: {} bytes in {} ms, ttl: {}'.format(bytes_received, time, ttl))
+        ping = Ping(host, time, ttl, bytes_received)
+        pings.append(ping)
 
 # with psycopg2.connect('dbname={} user={}'.format(user, user)) as conn:
 #     pass
--- pingmodel.py
+++ pingmodel.py
@@ -5,3 +5,7 @@ class Ping:
         self.ping_time = ping_time
         self.ttl = ttl
         self.bytes = bytes
+
+    def __repr__(self):
+        return 'PING: {} bytes to {} in {} ms, ttl: {}'\
+                 .format(self.bytes, self.destination, self.ping_time, self.ttl)
